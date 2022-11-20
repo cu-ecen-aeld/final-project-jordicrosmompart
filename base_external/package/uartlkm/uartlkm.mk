355feb1227868ea6b76defcb170a979cca2a3124
GuruprashanthKrishnakumar <gukr5411@colorado.edu>
2022-11-20 13:18:00 -0700

    Driver added

---
+++ base_external/package/uartlkm/uartlkm.mk
@@ -0,0 +1,20 @@
+
+##############################################################
+#
+# UARTLKM - GURUPRASHANTH KRISHNAKUMAR
+#
+##############################################################
+$(eval $(kernel-module))
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+UARTLKM_VERSION = 'a86d7f7196874a1c4b464b6de6ffcb68f1f383c8'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+UARTLKM_SITE = 'git@github.com:cu-ecen-aeld/final-project-GuruprashanthKrishnakumar.git'
+UARTLKM_SITE_METHOD = git
+UARTLKM_GIT_SUBMODULES = YES
+UARTLKM_MODULE_SUBDIRS = uart_driver
+
+
+
+$(eval $(generic-package))
