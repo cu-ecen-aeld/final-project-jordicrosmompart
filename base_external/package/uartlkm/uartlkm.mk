be88bdc6d0df97f0a049662bd74ce2b45a4d5b0c
Jordi <jordi.cros.mompart@gmail.com>
2022-11-29 06:50:35 +0000

    Test image

--- base_external/package/uartlkm/uartlkm.mk
+++ base_external/package/uartlkm/uartlkm.mk
@@ -6,7 +6,7 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-UARTLKM_VERSION = '78c4aafc9cd43da71569f0d83a3b64d423cdf7ca'
+UARTLKM_VERSION = '0955885a19b4f426bdd301b36121dc5ff2a8af11'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
