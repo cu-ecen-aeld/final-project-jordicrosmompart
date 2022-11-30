455d5dd4c0601aacd3bc482c427186db8a68f742
Jordi <jordi.cros.mompart@gmail.com>
2022-11-30 17:47:30 +0000

    Debugging read notify values

--- base_external/package/uartlkm/uartlkm.mk
+++ base_external/package/uartlkm/uartlkm.mk
@@ -6,7 +6,7 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-UARTLKM_VERSION = '4cb983d4f5781138d5b280497957a8ef2624baac'
+UARTLKM_VERSION = '4429dc88aad3ac527197b2dd72687b969918d235'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
