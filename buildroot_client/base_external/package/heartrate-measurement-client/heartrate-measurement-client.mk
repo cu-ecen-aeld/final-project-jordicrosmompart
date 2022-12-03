3770cea43b6f107d01e6c24234ae97fc82ed7e2e
GuruprashanthKrishnakumar <gukr5411@colorado.edu>
2022-12-02 21:37:28 -0700

    buildroot client image built

---
+++ buildroot_client/base_external/package/heartrate-measurement-client/heartrate-measurement-client.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# HEARTRATE_MEASUREMENT_CLIENT
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+HEARTRATE_MEASUREMENT_CLIENT_VERSION = '7b0e740cb60a0dd16db793609983c261227b32bb'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+HEARTRATE_MEASUREMENT_CLIENT_SITE = 'git@github.com:cu-ecen-aeld/final-project-GuruprashanthKrishnakumar.git'
+HEARTRATE_MEASUREMENT_CLIENT_SITE_METHOD = git
+HEARTRATE_MEASUREMENT_CLIENT_GIT_SUBMODULES = YES
+
+define HEARTRATE_MEASUREMENT_CLIENT_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_client all
+endef
+
+#Add writer, finder and finder-test utilities/scripts to the installation steps below
+define HEARTRATE_MEASUREMENT_CLIENT_INSTALL_TARGET_CMDS
+	
+	$(INSTALL) -m 0755 $(@D)/socket_client/socket_client $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(generic-package))
--- buildroot_client/base_external/package/uartlkm/uartlkm.mk
+++ buildroot_client/base_external/package/uartlkm/uartlkm.mk
@@ -6,7 +6,7 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-UARTLKM_VERSION = '170af67047fd82f95c978f3b387d5098de887a4a'
+UARTLKM_VERSION = '7b0e740cb60a0dd16db793609983c261227b32bb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,7 +14,5 @@ UARTLKM_SITE = 'git@github.com:cu-ecen-aeld/final-project-GuruprashanthKrishnaku
 UARTLKM_SITE_METHOD = git
 UARTLKM_GIT_SUBMODULES = YES
 UARTLKM_MODULE_SUBDIRS = uart_driver
-
-
 
 $(eval $(generic-package))
