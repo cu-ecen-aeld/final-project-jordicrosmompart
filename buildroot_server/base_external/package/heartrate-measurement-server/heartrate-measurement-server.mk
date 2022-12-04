d4e4689af5bc630d33d43bd3f47097c9cfa7ebea
Jordi <jordi.cros.mompart@gmail.com>
2022-12-04 17:13:53 +0000

    Debugging communciation between server and client

--- buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
+++ buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-HEARTRATE_MEASUREMENT_SERVER_VERSION = '96a19eac33a3fa4e2ee2d700912d6dc3dcc46d4d'
+HEARTRATE_MEASUREMENT_SERVER_VERSION = '6cb7b49797b84e71a93bb345d9e04e0f663f1893'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
