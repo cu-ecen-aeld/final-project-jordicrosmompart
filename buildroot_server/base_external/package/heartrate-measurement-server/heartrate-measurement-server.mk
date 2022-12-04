9791db2d0e5ad3ea5f3aa8835c1fa5a9bb0ce218
Jordi <jordi.cros.mompart@gmail.com>
2022-12-04 22:07:33 +0000

    Testing server/client coomms

--- buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
+++ buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-HEARTRATE_MEASUREMENT_SERVER_VERSION = '0de19594a85e4ab37aaa7c5a711969a74d1bccd5'
+HEARTRATE_MEASUREMENT_SERVER_VERSION = 'f0cdc5f437a2a2fd3fbf88c45c13ae8b36c873d3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
