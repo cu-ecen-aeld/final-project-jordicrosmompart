1d52ee87448b73154c117f7b1e841fac1804d8a6
Jordi <jordi.cros.mompart@gmail.com>
2022-12-04 22:24:01 +0000

    Final image

--- buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
+++ buildroot_server/base_external/package/heartrate-measurement-server/heartrate-measurement-server.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-HEARTRATE_MEASUREMENT_SERVER_VERSION = 'eb3135699f3d217d9d85c25d8302a17c99be33da'
+HEARTRATE_MEASUREMENT_SERVER_VERSION = '50c4776bed2c89014ed0949b383f0573296557de'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- buildroot_server/base_external/package/uartlkm/uartlkm.mk
+++ buildroot_server/base_external/package/uartlkm/uartlkm.mk
@@ -6,7 +6,7 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-UARTLKM_VERSION = 'eb3135699f3d217d9d85c25d8302a17c99be33da'
+UARTLKM_VERSION = '50c4776bed2c89014ed0949b383f0573296557de'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
