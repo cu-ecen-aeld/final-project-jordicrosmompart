fdb607ad01520f2edbc864f7062cc4f4b5a69788
Jordi <jordi.cros.mompart@gmail.com>
2022-12-05 04:34:25 +0000

    Debugging extra features

--- buildroot_server/base_external/package/hm11lkm/hm11lkm.mk
+++ buildroot_server/base_external/package/hm11lkm/hm11lkm.mk
@@ -7,7 +7,7 @@
 $(eval $(kernel-module))
 HM11LKM_DEPENDENCIES += uartlkm
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-HM11LKM_VERSION = '6da2cbc31e8082deb0ca770693e5197362b793fd'
+HM11LKM_VERSION = '25ee63275db7e206dc95a5ce71aee36ce8f23b73'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
